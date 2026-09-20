0d429ff7bceae2b6d65b81dc97327eda3663091d
Shush <almogosheriko@gmail.com>
2026-09-20 19:29:19 +0300

    Respect Tooltips user settings in Options

--- src/new/battleui/battleui.cpp
+++ src/new/battleui/battleui.cpp
@@ -194,7 +194,7 @@ HRESULT BattleUISystem::Load(IStream *pStm)
     hr = Power.Load(pStm);
     if (FAILED(hr)) return hr;
 
-    ToolTips->Set_Active(true);
+    ToolTips->Set_Active(Options.ToolTips);
 
     return S_OK;
 }
